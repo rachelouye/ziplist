23061cf0222ee6f69a7e6078b1350e3d2003863d
rachelouye <ro7@hawaii.edu>
2024-02-05 20:58:18 -1000

    This wod took 23 minutes

---
+++ ziplist.js
@@ -0,0 +1,15 @@
+const list1 = ['a','b','c'];
+const list2 = [1,2,3];
+
+function zipList (first,second) {
+  const retList = [];
+  for (let i = 0; i < first.length; i++) {
+    retList.push(first[i], second[i]);
+  }
+  return retList;
+}
+console.log(ziplist(list1,list2));
+
+const zipListTheSimpleWay = (first,second) => _.flatten(_.zip(first,second));
+
+console.log(zipListTheSimpleWay(list1,list2));
